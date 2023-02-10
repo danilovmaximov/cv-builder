cede9d396e3ea760c6b6457819b62588113bbdee
Dan Maksimov <danilmaksimov@Dans-MacBook-Pro.local>
2023-02-11 00:30:02 +0100

    Fixed URI injection vulnerability

--- src/main/java/com/cvbuilder/controller/IntegrationsRestController.java
+++ src/main/java/com/cvbuilder/controller/IntegrationsRestController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,7 +29,8 @@ public class IntegrationsRestController {
 
     @GetMapping(value = "/repos")
     private List<Object> getUsersRepos(String username) {
-        String uri = String.format("https://api.github.com/users/%s/repos", username);
+        URI uri = UriComponentsBuilder.fromHttpUrl("https://api.github.com/users/{username}/repos")
+                .build(username);
         Object[] result = restTemplate.getForObject(uri, Object[].class);
         return result != null ? List.of(result) : null;
     }
